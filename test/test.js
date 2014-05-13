2d60a79060db41f4a5d63ac23531416a58f1d5ee
zk33 <ymzktkak@gmail.com>
2014-05-13 14:58:24 +0900

    update test

--- test/test.js
+++ test/test.js
@@ -48,11 +48,10 @@ describe('Wig', function(){
       dataDir: data,
       tmplDir: template,
       outDir: dist,
-      verbose: true
+      verbose: false
     }
     if(fs.existsSync(dist)){
       rimraf.sync(dist);
-
     }
   });
   describe('build()', function(){
@@ -69,8 +68,37 @@ describe('Wig', function(){
     it('should load datas and create files', function(){
       var w = new Wig(options);
       w.build();
+      
+      //pages
+      var index = path.join(dist,'index.html');
+      var php = path.join(dist,'php.php');
+      var sub = path.join(dist,'sub.html');
+      var pagefile = path.join(dist,'page-as-file.html');
+      var deep = path.join(dist,'dir/deep-page.html');
+      var quiteDeep = path.join(dist,'dir/dir2/quite-deep-page.html');
+
+      assert(fs.existsSync(index),'index.html should generated');
+      assert(fs.existsSync(php),'_ext parameter should override extension and should output php.php');
+      assert(fs.existsSync(sub),'sub.html should generated');
+      assert(fs.existsSync(pagefile),'page-as-file.html should generated via data/page-as-file.json');
+      assert(fs.existsSync(deep),'dir/deep-page.html should generated via data/__init.json');
+      assert(fs.existsSync(quiteDeep),'dir/dir2/quite-deep-page.html should generated via data/__init__.json');
+
+      //content
+      var indexContents = fs.readFileSync(index,{encoding:'utf8'}).split("\n");
+      var subContents = fs.readFileSync(sub,{encoding:'utf8'}).split("\n");
+      var quiteDeepContents = fs.readFileSync(quiteDeep,{encoding:'utf8'}).split("\n");
+
+      //data inheritance
+      assert.equal(indexContents[0],'index','data properly setted in index.html');
+      assert.equal(quiteDeepContents[0],'quite deep page','data properly setted in dir/dir2/quite-deep-page.html');
+      assert.equal(quiteDeepContents[6],'this is text','data properly inherited in dir/dir2/quite-deep-page.html');
+
+      //template finder
+      assert.equal(quiteDeepContents[2],'base in dir','dir/dir2/quite-deep-page.html should use dir.html');
+
+
+
     });
   });
 });
-
-
