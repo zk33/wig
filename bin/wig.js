7a939a87a4d59dd575143c6bfda3e82403b44a22
zk33 <ymzktkak@gmail.com>
2016-03-06 16:17:00 +0900

    ソース整形

--- bin/wig.js
+++ bin/wig.js
@@ -6,11 +6,11 @@ var cli = require('commander');
 
 cli.version('0.1.2')
 
-function makeObj(str){
+function makeObj(str) {
   var arr = str.split(',');
   var res = {};
-  for(var i=0,len=arr.length;i<len;i+=2){
-    res[arr[i]] = arr[i+1];
+  for (var i = 0, len = arr.length; i < len; i += 2) {
+    res[arr[i]] = arr[i + 1];
   }
   return res;
 }
@@ -22,20 +22,32 @@ cli.command('build')
   .option('-o, --out_dir <path>', 'Output dirctory (default:./dist)')
   .option('-v, --verbose', 'Display rendered files names')
   .option('-r, --renderer <name>', 'Renderer (default:nunjucks)')
-  .option('-a, --assign <items>', 'Assign template vars (KEY,VALUE,KEY,VALUE...)(default:"")',makeObj)
-  .action(function(cmd){
+  .option('-a, --assign <items>', 'Assign template vars (KEY,VALUE,KEY,VALUE...)(default:"")', makeObj)
+  .action(function(cmd) {
 
     // setup options
     var opt = {};
 
     opt.rootDir = process.cwd();
 
-    if(cmd.data_dir){ opt.dataDir = cmd.data_dir; }
-    if(cmd.tmpl_dir){ opt.tmplDir = cmd.tmpl_dir; }
-    if(cmd.out_dir){ opt.outDir = cmd.out_dir; }
-    if(cmd.verbose){ opt.verbose = cmd.verbose; }
-    if(cmd.renderer){ opt.renderer = cmd.renderer; }
-    if(cmd.assign){ opt.vars = cmd.assign; }
+    if (cmd.data_dir) {
+      opt.dataDir = cmd.data_dir;
+    }
+    if (cmd.tmpl_dir) {
+      opt.tmplDir = cmd.tmpl_dir;
+    }
+    if (cmd.out_dir) {
+      opt.outDir = cmd.out_dir;
+    }
+    if (cmd.verbose) {
+      opt.verbose = cmd.verbose;
+    }
+    if (cmd.renderer) {
+      opt.renderer = cmd.renderer;
+    }
+    if (cmd.assign) {
+      opt.vars = cmd.assign;
+    }
 
     // build
     var Wig = require('../lib');
